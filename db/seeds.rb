300217116a747f675f02ac81f91ae1198b9a5c38
Ethan <eleetyson@gmail.com>
2020-11-24 12:49:56 -0500

    tweak seed data structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-i1 = Idea.find_by(title: "Title goes here") || Idea.create(title: "Title goes here", content: "This is the idea. Another sentence.", source: "RFS 100 newsletter", link: "www.link.com")
-i2 = Idea.find_by(title: "Second idea title here") || Idea.create(title: "Second idea title here", content: "I have an idea! Let me explain this idea...", source: "My First Million podcast", link: "www.anotherlink.com")
+i1 = Idea.find_by(title: "Title goes here") || Idea.create(title: "Title goes here", content: "This is the idea. Another sentence.", source: "RFS 100 newsletter", link: "https://www.link.com")
+i2 = Idea.find_by(title: "Second idea title here") || Idea.create(title: "Second idea title here", content: "I have an idea! Let me explain this idea...", source: "My First Million podcast", link: "https://www.anotherlink.com")
