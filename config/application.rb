41e3c4f5f9094471af2354d6a04b3e43038f80a8
Ethan <eleetyson@gmail.com>
2020-11-30 10:53:47 -0500

    remove test from Application.rb

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,5 @@ module RfyesBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
   end
 end
